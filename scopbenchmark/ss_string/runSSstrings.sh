58ccbc2039181297edb019acfbca5ca714ea3fb7
Martin <s0794732@bioinfws22.biotec.tu-dresden.de>
2022-07-15 18:21:51 +0200

    update

--- scopbenchmark/ss_string/runSSstrings.sh
+++ scopbenchmark/ss_string/runSSstrings.sh
@@ -6,44 +6,19 @@ align() {
   date
 
   ## mmseqs search module
-  mmseqs easy-search "$1" "$1" ./alignResults/rawoutput/no_matrix/"$dbname".m8 tmp  \
-    -a --threads 24 -s 7.5 -e 10000 --max-seqs 2000  --sub-mat SSAlign_large_negative.out --seed-sub-mat SSAlign_large_negative.out
+  mmseqs easy-search "$1" "$1" ./alignResults/rawoutput/"$dbname".m8 tmp  \
+    -a --threads 32 -s 7.5 -e 10000 --max-seqs 2000  --sub-mat SSAlign_large_negative.out --seed-sub-mat SSAlign_large_negative.out > ./alignResults/logs/"$dbname".log
 
   ## end timing
   date
 
   ## generate ROCX file
-  ./bench.awk scop_lookup.tsv <(cat ./alignResults/rawoutput/no_matrix/"$dbname".m8) > ./alignResults/rocx/no_matrix/"$dbname".rocx
+  ./bench.awk scop_lookup.tsv <(cat ./alignResults/rawoutput/"$dbname".m8) > ./alignResults/rocx/"$dbname".rocx
 
   ## calculate auc
-   awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' ./alignResults/rocx/no_matrix/"$dbname".rocx
-}
-
-align_ex() {
-  dbname=$(basename "$1" .fasta)_ex
-
-  ## start timing
-  date
-
-  ## mmseqs search module
-  mmseqs easy-search "$1" "$1" ./alignResults/rawoutput/no_matrix/"$dbname".m8 tmp  \
-    -a --threads 24 -s 7.5 -e 10000 --max-seqs 2000  --sub-mat SSAlign_large_negative.out --seed-sub-mat SSAlign_large_negative.out \
-    --exhaustive-search
-
-  ## end timing
-  date
-
-  ## generate ROCX file
-  ./bench.awk scop_lookup.tsv <(cat ./alignResults/rawoutput/no_matrix/"$dbname".m8) > ./alignResults/rocx/no_matrix/"$dbname".rocx
-
-  ## calculate auc
-   awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' ./alignResults/rocx/no_matrix/"$dbname".rocx
+   awk '{ famsum+=$3; supfamsum+=$4; foldsum+=$5}END{print famsum/NR,supfamsum/NR,foldsum/NR}' ./alignResults/rocx/"$dbname".rocx
 }
 
 align ss_strings_dssp.fasta
 align ss_strings_dssp_nc.fasta
-
-align_ex ss_strings_dssp.fasta
-align_ex ss_strings_dssp_nc.fasta
-
 
