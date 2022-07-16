57f8eca7fc264038b7c9a2d28c94dc25fcc1b4a1
martin <mmenzel98@web.de>
2022-07-16 14:33:53 +0200

    update

--- scopbenchmark/ss_string/runSSstrings_.sh
+++ scopbenchmark/ss_string/runSSstrings_.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./runSSString ss_strings_dssp.fasta
-./runSSString ss_strings_dssp_nc.fasta
+./runSSString.sh ss_strings_dssp.fasta
+./runSSString.sh ss_strings_dssp_nc.fasta
 
-./runSSString ss_strings_dssp.fasta mat_sub_dssp.out mat_sub_dssp.out
-./runSSString ss_strings_dssp.fasta mat_sub_dssp.out mat_1.out
-./runSSString ss_strings_dssp.fasta mat_1.out mat_sub_dssp.out
+./runSSString.sh ss_strings_dssp.fasta mat_sub_dssp.out mat_sub_dssp.out
+./runSSString.sh ss_strings_dssp.fasta mat_sub_dssp.out mat_1.out
+./runSSString.sh ss_strings_dssp.fasta mat_1.out mat_sub_dssp.out
 
